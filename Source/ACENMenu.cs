f35df2e2af19b1982e881d0e3daf844254d86a4c
Dingo <oferdottan@gmail.com>
2016-10-29 17:04:40 +0300

    Initial release

---
+++ Source/ACENMenu.cs
@@ -0,0 +1,29 @@
+﻿using HugsLib;
+using AutocloseEN;
+
+public class ACENMenu : ModBase
+{
+    public override string ModIdentifier
+    {
+        get
+        {
+            return "AutocloseEventNotifications";
+        }
+    }
+
+    public override void DefsLoaded()
+    {
+        UpdateDefs();
+    }
+
+    public override void SettingsChanged()
+    {
+        UpdateDefs();
+    }
+
+    private void UpdateDefs()
+    {
+        var showMessage = Settings.GetHandle<bool>("ACENShowMessage", "Show a message when removing notifications", "A silent message will be dispalyed when removing an old non-urgent notification.", false);
+        AutocloseEventBoxes.ShowMessage = showMessage.Value;
+    }
+}
